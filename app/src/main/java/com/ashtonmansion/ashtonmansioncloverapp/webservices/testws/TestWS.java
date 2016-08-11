c09b64bcecab9d97c6e6e9529426260cf9d837e5
pcurtis5688 <pcurtis5688@gmail.com>
2016-08-10 23:35:10 -0400

    WORKING SOAP CALLS TO DYNAMICS FROM WITHIN THE APPLICATION. REVERT HERE IF NEED BE

--- app/src/main/java/com/ashtonmansion/ashtonmansioncloverapp/webservices/testws/TestWS.java
+++ app/src/main/java/com/ashtonmansion/ashtonmansioncloverapp/webservices/testws/TestWS.java
@@ -70,67 +70,67 @@ public class TestWS {
             /////////////
             ///PARAMS////
             PropertyInfo pi1 = new PropertyInfo();
-            pi1.setName("pID");
+            pi1.setName("iD");
             pi1.setValue(appt.get_id());
             pi1.setType(String.class);
             request.addProperty(pi1);
 
             PropertyInfo pi2 = new PropertyInfo();
-            pi2.setName("pDate");
+            pi2.setName("date");
             pi2.setValue(appt.get_date());
             pi2.setType(String.class);
             request.addProperty(pi2);
 
             PropertyInfo pi3 = new PropertyInfo();
-            pi3.setName("pTime");
+            pi3.setName("time");
             pi3.setValue(appt.get_start_time());
             pi3.setType(String.class);
             request.addProperty(pi3);
 
             PropertyInfo pi4 = new PropertyInfo();
-            pi4.setName("pCustCode");
+            pi4.setName("customer_Code");
             pi4.setValue(appt.get_customer_code());
             pi4.setType(String.class);
             request.addProperty(pi4);
 
             PropertyInfo pi5 = new PropertyInfo();
-            pi5.setName("pDuration");
+            pi5.setName("duration");
             pi5.setValue(appt.get_duration());
             pi5.setType(String.class);
             request.addProperty(pi5);
 
             PropertyInfo pi6 = new PropertyInfo();
-            pi6.setName("palerttype");
+            pi6.setName("alert_Type");
             pi6.setValue(appt.get_alert_type());
             pi6.setType(String.class);
             request.addProperty(pi6);
 
             PropertyInfo pi7 = new PropertyInfo();
-            pi7.setName("pItemCode");
+            pi7.setName("item_Code");
             pi7.setValue(appt.get_item_code());
             pi7.setType(String.class);
             request.addProperty(pi7);
 
             PropertyInfo pi8 = new PropertyInfo();
-            pi8.setName("pNotes");
+            pi8.setName("notes");
             pi8.setValue(appt.get_note());
             pi8.setType(String.class);
             request.addProperty(pi8);
 
             PropertyInfo pi9 = new PropertyInfo();
-            pi9.setName("pEmpl1");
+            pi9.setName("employee_1");
             pi9.setValue(appt.get_employee_code_1());
             pi9.setType(String.class);
             request.addProperty(pi9);
 
             PropertyInfo pi10 = new PropertyInfo();
-            pi10.setName("pEmpl2");
+            pi10.setName("employee_2");
             pi10.setValue(appt.get_employee_code_2());
             pi10.setType(String.class);
             request.addProperty(pi10);
 
             PropertyInfo pi11 = new PropertyInfo();
-            pi11.setName("pConfStatus");
+            pi11.setName("confirmation_Status");
             pi11.setValue(appt.get_confirm_status());
             pi11.setType(String.class);
             request.addProperty(pi11);
@@ -138,7 +138,7 @@ public class TestWS {
             NTLMTransport transport = new NTLMTransport();
             transport.setCredentials(APPT_WEB_SERVICE_URL, "paul", "Wmo67766767", "laptop-53b1c7v6", "");
             transport.call(APPT_WS_SOAP_ACTION, envelope);
-            response = transport.responseDump;
+            response = transport.requestDump;
             Log.i("Result: ", "" + response);
         } catch (IOException e1) {
             Log.e("IO Err: ", "" + "" + e1.getMessage());
