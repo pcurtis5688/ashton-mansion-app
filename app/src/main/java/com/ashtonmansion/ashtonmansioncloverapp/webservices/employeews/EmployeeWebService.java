e3fc86e9c9f774fa2a886f88eb9f5cd367e8f013
pcurtis5688 <pcurtis5688@gmail.com>
2016-08-11 07:12:26 -0400

    [11 Aug early am changes] addition of EmployeeWebService

---
+++ app/src/main/java/com/ashtonmansion/ashtonmansioncloverapp/webservices/employeews/EmployeeWebService.java
@@ -0,0 +1,60 @@
+package com.ashtonmansion.ashtonmansioncloverapp.webservices.employeews;
+
+import android.util.Log;
+
+import com.ashtonmansion.ashtonmansioncloverapp.dbo.Appointment;
+import com.ashtonmansion.ashtonmansioncloverapp.webservices.generalws.WebServiceUtilities;
+import com.ashtonmansion.ashtonmansioncloverapp.webservices.generalws.external.NTLMTransport;
+
+import org.ksoap2.serialization.PropertyInfo;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
+/**
+ * Created by paul on 8/11/2016.
+ */
+public class EmployeeWebService {
+    // STATIC SERVICE VARS
+    ///////////////////////
+    private static final String EMPL_WS_NAMESPACE = "urn:microsoft-dynamics-schemas/codeunit/EmployeeWebService";
+    private static final String EMPL_WS_SOAP_ACTION = "urn:microsoft-dynamics-schemas/codeunit/EmployeeWebService:CreateEmployee";
+    private static final String EMPL_WEB_SERVICE_URL = "http://10.0.3.2:7047/DynamicsNAV90/WS/CRONUS%20Canada,%20Inc./Codeunit/EmployeeWebService";
+    private static final String CREATE_EMPL_METHOD = "CreateEmployee";
+
+    public boolean createEmployeeServiceCall(Appointment appt) {
+        boolean resultBool = true;
+        try {
+            SoapObject request = new SoapObject(EMPL_WS_NAMESPACE, CREATE_EMPL_METHOD);
+            SoapSerializationEnvelope envelope = WebServiceUtilities.getSoapSerializationEnvelope(request);
+            /////////////
+            ///PARAMS////
+            PropertyInfo pi1 = new PropertyInfo();
+            pi1.setName("iD");
+            pi1.setValue(appt.get_id());
+            pi1.setType(String.class);
+            request.addProperty(pi1);
+
+            ////////////MAKE THE CALL
+            NTLMTransport transport = new NTLMTransport();
+            transport.setCredentials(EMPL_WEB_SERVICE_URL, "paul", "Wmo67766767", "laptop-53b1c7v6", "");
+            transport.call(EMPL_WS_SOAP_ACTION, envelope);
+            //TODO HERE THE WEB SERVICE SENDS BACK IF SUCCESSFUL OR NOT, AND I EVALUATE
+            String requestDump = transport.requestDump;
+            Log.i("Result: ", "" + requestDump);
+        } catch (IOException e1) {
+            Log.e("IO Err: ", "" + "" + e1.getMessage());
+            resultBool = false;
+        } catch (XmlPullParserException e2) {
+            Log.e("XMLPull Excpt: ", "" + e2.getMessage());
+            resultBool = false;
+        } catch (Exception e) {
+            Log.e("Exception in: ", "" + e.getMessage());
+            resultBool = false;
+        }
+
+        return resultBool;
+    }
+}
