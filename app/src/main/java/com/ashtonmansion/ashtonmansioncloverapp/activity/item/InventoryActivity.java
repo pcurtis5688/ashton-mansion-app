654b87031e75b45f5cb5123affb913a914d9005e
pcurtis5688 <pcurtis5688@gmail.com>
2016-08-21 11:39:07 -0400

    21 august - lunch

--- app/src/main/java/com/ashtonmansion/ashtonmansioncloverapp/activity/item/InventoryActivity.java
+++ app/src/main/java/com/ashtonmansion/ashtonmansioncloverapp/activity/item/InventoryActivity.java
@@ -1,6 +1,7 @@
 package com.ashtonmansion.ashtonmansioncloverapp.activity.item;
 
 import android.accounts.Account;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
@@ -18,28 +19,13 @@ import com.clover.sdk.v3.inventory.Item;
 import java.util.List;
 
 public class InventoryActivity extends AppCompatActivity {
-    // Private account vars
+    // PRIVATE ACTIVITY VARS
     private Account mAcct;
     private InventoryConnector mInvConn;
     private List<Item> itemList;
-    //Inventory view vars
+    private Context inventoryActivityContext;
+    //ITEM TABLE OBJECT
     private TableLayout itemTable;
-    private TableRow itemHeaderRow;
-    private TableRow newItemRow;
-    private TextView itemNameTextview;
-    private TextView itemModifierTextview;
-    private TextView itemProductCodeTextview;
-    private TextView itemSkuTextview;
-    private TextView itemPriceTextview;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inventory);
-
-        itemTable = (TableLayout) findViewById(R.id.item_table);
-    }
-
 
     private void getInventoryList() {
         new AsyncTask<Void, Void, Item>() {
@@ -64,39 +50,40 @@ public class InventoryActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(Item item) {
                 populateItemTable();
-                populateItemDetail();
             }
         }.execute();
     }
 
     private void populateItemTable() {
-        //Clear the item table
+        //CLEAR INVENTORY/ITEM TABLE
         itemTable.removeAllViews();
-        // todo Create item header row
+        //CREATE HEADER ROW
         createItemTableHeaderRow();
+        TableRow newItemRow = new TableRow(this);
         //Create new row to add to the table
         if (itemList != null && itemList.size() > 0) {
             for (Item item : itemList) {
-                TableRow newItemRow = new TableRow(this);
-                //Create new views to populate data
-                itemNameTextview = new TextView(this);
-                itemModifierTextview = new TextView(this);
-                itemProductCodeTextview = new TextView(this);
-                itemSkuTextview = new TextView(this);
-                itemPriceTextview = new TextView(this);
-                //Set the new data for the new row
+                //CREATE NEW INVENTORY ROW
+                newItemRow = new TableRow(this);
+                //CREATE NEW INVENTORY COLUMN VIEWS
+                TextView itemNameTextview = new TextView(this);
+                TextView itemModifierTextview = new TextView(this);
+                TextView itemProductCodeTextview = new TextView(this);
+                TextView itemSkuTextview = new TextView(this);
+                TextView itemPriceTextview = new TextView(this);
+                //POPULATE TEXT FIELDS WITH DATA
                 itemNameTextview.setText(item.getName());
                 itemModifierTextview.setText(item.getModifierGroups().toString());
                 itemProductCodeTextview.setText(item.getCode());
                 itemSkuTextview.setText(item.getSku());
                 itemPriceTextview.setText(item.getPrice().toString());
-                //Add the new data to the new row
+                //ADD DATA TO THE NEW ROW
                 newItemRow.addView(itemNameTextview);
                 newItemRow.addView(itemModifierTextview);
                 newItemRow.addView(itemProductCodeTextview);
                 newItemRow.addView(itemSkuTextview);
                 newItemRow.addView(itemPriceTextview);
-                //Add the new row to the table
+                //ADD THE NEW ROW TO THE TABLE
                 itemTable.addView(newItemRow);
             }
         } else {
@@ -106,21 +93,59 @@ public class InventoryActivity extends AppCompatActivity {
         }
     }
 
-    private void populateItemDetail() {
-        //Todo decide on this implementation
-         /* Item testItem = itemList.get(0);
-        TextView itemDetailNameTv = (TextView) findViewById(R.id.item_detail_name);
-        itemDetailNameTv.setText(testItem.getName());
-
-        String testData = "Testing data: " + testItem.getName();
-        itemDetailNameTv.setText(testData);*/
-    }
-
     private void createItemTableHeaderRow() {
-        //TODO THIS
+        //CREATE HEADER ROW
+        TableRow inventoryTableHeaderRow = new TableRow(inventoryActivityContext);
+        //CREATE HEADER TEXT VIEWS
+        TextView itemNameHeaderTV = new TextView(inventoryActivityContext);
+        TextView itemModifierHeaderTV = new TextView(inventoryActivityContext);
+        TextView itemProductCodeHeaderTV = new TextView(inventoryActivityContext);
+        TextView itemSkuHeaderTV = new TextView(inventoryActivityContext);
+        TextView itemPriceHeaderTV = new TextView(inventoryActivityContext);
+        //SET TEXT OF HEADERS
+        itemNameHeaderTV.setText(getResources().getString(R.string.inventory_table_name_header_str));
+        itemModifierHeaderTV.setText(getResources().getString(R.string.inventory_table_modifier_header_str));
+        itemProductCodeHeaderTV.setText(getResources().getString(R.string.inventory_table_product_code_header_str));
+        itemSkuHeaderTV.setText(getResources().getString(R.string.inventory_table_sku_header_str));
+        itemPriceHeaderTV.setText(getResources().getString(R.string.inventory_table_price_header_str));
+        //ADD THE FIELDS TO THE NEW ROW
+        inventoryTableHeaderRow.addView(itemNameHeaderTV);
+        inventoryTableHeaderRow.addView(itemModifierHeaderTV);
+        inventoryTableHeaderRow.addView(itemProductCodeHeaderTV);
+        inventoryTableHeaderRow.addView(itemSkuHeaderTV);
+        inventoryTableHeaderRow.addView(itemPriceHeaderTV);
+        //ADD THE NEW ROW TO THE TABLE
+        itemTable.addView(inventoryTableHeaderRow);
     }
 
-    /*ITEMS BELOW ARE COMPLETE *///////////////////////
+    private void connectInventory() {
+        disconnectInventory();
+        if (mAcct != null) {
+            mInvConn = new InventoryConnector(this, mAcct, null);
+            mInvConn.connect();
+        }
+    }
+
+    private void disconnectInventory() {
+        if (mInvConn != null) {
+            mInvConn.disconnect();
+            mInvConn = null;
+        }
+    }
+
+    public void displayAddInventory(View view) {
+        Intent addInventoryItemIntent = new Intent(this, AddInventoryActivity.class);
+        startActivity(addInventoryItemIntent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+        inventoryActivityContext = this;
+        itemTable = (TableLayout) findViewById(R.id.item_table);
+    }
+
     @Override
     protected void onPause() {
         disconnectInventory();
@@ -144,25 +169,5 @@ public class InventoryActivity extends AppCompatActivity {
         //Connect the Merchant Inventory Connector
         connectInventory();
         getInventoryList();
-    }
-
-    private void connectInventory() {
-        disconnectInventory();
-        if (mAcct != null) {
-            mInvConn = new InventoryConnector(this, mAcct, null);
-            mInvConn.connect();
-        }
-    }
-
-    private void disconnectInventory() {
-        if (mInvConn != null) {
-            mInvConn.disconnect();
-            mInvConn = null;
-        }
-    }
-
-    public void displayAddInventory(View view) {
-        Intent addInventoryItemIntent = new Intent(this, AddInventoryActivity.class);
-        startActivity(addInventoryItemIntent);
     }
 }
