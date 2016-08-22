66200da43df0d54bdb2beb8af71a2af3e3eada91
pcurtis5688 <pcurtis5688@gmail.com>
2016-08-22 13:16:37 -0400

    22 august final edit employee page

--- app/src/main/java/com/ashtonmansion/ashtonmansioncloverapp/activity/employee/EditEmployeeActivity.java
+++ app/src/main/java/com/ashtonmansion/ashtonmansioncloverapp/activity/employee/EditEmployeeActivity.java
@@ -2,6 +2,7 @@ package com.ashtonmansion.ashtonmansioncloverapp.activity.employee;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -15,13 +16,21 @@ import com.clover.sdk.v1.customer.Customer;
  */
 public class EditEmployeeActivity extends AppCompatActivity {
 
+
+    public void finalizeEmployeeModification(View view) {
+
+    }
+
+    public void cancelEditEmployee(View view) {
+        finish();
+    }
+
+    /////////ACTIVITY FLOW METHODS ////////////////////////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_employee);
     }
-
-    //////* BELOW METHODS ARE BASICALLY COMPLETE *///////////
 
     @Override
     protected void onResume() {
